ed0c633bbfd55e3be54ceaca5fdb84a015a9ec59
Ronnie Woo <rwoo50@stuy.edu>
2025-01-06 21:27:59 -0500

    added sighandling

--- persistant_server.c
+++ persistant_server.c
@@ -4,7 +4,24 @@
 int to_client;
 int from_client;
 
+void siginthandler(int sig){
+    printf("(SERVER) Closing server and WKP\n");
+    int terminate = -1;
+    if (write(to_client, &terminate, sizeof(terminate)) != -1){
+        close(to_client);
+    }
+    close(from_client);
+    remove(WKP);
+    exit(0);
+}
+
+void sigpipehandler(int sig){
+    printf("(SERVER) A client disconnected (SIGPIPE)\n");
+}
+
 int main() {
+    signal(SIGINT, siginthandler);
+    signal(SIGPIPE, sigpipehandler);
   while (1){
     from_client = server_handshake(&to_client);
     printf("(SERVER) Connection established\n");
